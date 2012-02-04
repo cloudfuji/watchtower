0b84b159734e7deaf7040454690adc24ecb1b256
Kev Zettler <kevzettler@gmail.com>
2012-02-03 17:49:08 -0800

    added service observer to fire bushido events on recover and ciritical

---
+++ app/models/service_observer.rb
@@ -0,0 +1,24 @@
+class ServiceObserver < ActiveRecord::Observer
+  observe :service
+  def after_update(model)
+    if model.status_changed?
+    
+      if (model.status == 500 or model.status == 404) and model.status_was == 200
+        Bushido::Event.publish({
+                                 :category => :watchtower,
+                                 :name => :critical,
+                                 :data => model.to_json
+                               })
+      end
+
+      if model.status == 200 and (model.status == 500 or model.status == 404)
+                Bushido::Event.publish({
+                                 :category => :watchtower,
+                                 :name => :recovery,
+                                 :data => model.to_json
+                               })
+      end
+
+    end
+  end
+end
