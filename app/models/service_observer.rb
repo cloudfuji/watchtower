b0929ca281df826c8c4ef45c24c961b1af975b4b
Sean Grove <s@bushi.do>
2012-02-22 10:18:58 -0800

    Publish events with human keys for kogo/moldiboard/etc. consumption

--- app/models/service_observer.rb
+++ app/models/service_observer.rb
@@ -4,18 +4,23 @@ class ServiceObserver < ActiveRecord::Observer
     if model.status_changed?
     
       if (model.status == 500 or model.status == 404) and model.status_was == 200
+        data = model.to_hash
+        data[:human] = "#{model.service_url} has gone critical!"
         Bushido::Event.publish({
-                                 :category => :watchtower,
+                                 :category => :site_monitor,
                                  :name => :critical,
-                                 :data => model.to_json
+                                 :data => data
                                })
       end
 
       if model.status == 200 and (model.status_was == 500 or model.status_was == 404)
-                Bushido::Event.publish({
-                                 :category => :watchtower,
-                                 :name => :recovery,
-                                 :data => model.to_json
+        data = model.to_hash
+        data[:human] = "#{model.service_url} has recovered."
+
+        Bushido::Event.publish({
+                                 :category => :site_monitor,
+                                 :name => :recovered,
+                                 :data => data
                                })
       end
 
