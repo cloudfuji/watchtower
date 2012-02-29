6dec554dbcbdc3374e62341bb3d05380ef817036
Sean Grove <s@bushi.do>
2012-02-28 21:10:08 -0800

    Fixes minor typo and checks service status against string statuses isntead of integers

--- app/models/service.rb
+++ app/models/service.rb
@@ -10,22 +10,22 @@ class Service < ActiveRecord::Base
       uri = URI(self.service_url)
       response = Net::HTTP.get_response(uri)
       puts "status: #{response.code}"
-      self.status = new_status = response.code 
+      self.status = new_status = response.code
     rescue => e
       puts "Ping Exception"
       puts e.inspect
-      self.status = 500
+      self.status = "500"
     end
 
-    puts "Service: #{service.inspect}"
+    puts "Service: #{self.inspect}"
 
     self.pinged_at = Time.now
     self.save
 
     # There's been a status transition
     if new_status != current_status
-      notify_observers(:service_recovered) if status == 200
-      notify_observers(:service_downed)    if status != 200
+      notify_observers(:service_recovered) if status == "200"
+      notify_observers(:service_downed)    if status != "200"
     end
   end
 end
