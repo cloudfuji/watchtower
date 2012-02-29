b753d83695a6c4ee2f30d58d9cd905248754da0d
Sean Grove <s@bushi.do>
2012-02-28 20:04:55 -0800

    Updates the service_observers to modularize the events published

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,20 +3,27 @@ class Service < ActiveRecord::Base
   def ping
     puts "pinging service"
     puts self.service_url
+
+    current_status = self.status
+
     begin
       uri = URI(self.service_url)
       response = Net::HTTP.get_response(uri)
-      puts "status"
-      puts response.code
-      self.status = response.code
+      puts "status: #{response.code}"
+      self.status = new_status = response.code 
     rescue Exception => ex
       puts "Ping Exception"
       puts ex
       self.status = 500
     end
+
     self.pinged_at = Time.now
     self.save
+
+    # There's been a status transition
+    if new_status != current_status
+      notify_observers(:service_recovered) if status == 200
+      notify_observers(:service_downed)    if status != 200
+    end
   end
-
-
 end
--- app/models/service_observer.rb
+++ app/models/service_observer.rb
@@ -1,29 +1,26 @@
 class ServiceObserver < ActiveRecord::Observer
   observe :service
-  def after_update(model)
-    if model.status_changed?
-    
-      if (model.status == 500 or model.status == 404) and model.status_was == 200
-        data = model.to_hash
-        data[:human] = "#{model.service_url} has gone critical!"
-        Bushido::Event.publish({
-                                 :category => :site_monitor,
-                                 :name => :critical,
-                                 :data => data
-                               })
-      end
 
-      if model.status == 200 and (model.status_was == 500 or model.status_was == 404)
-        data = model.to_hash
-        data[:human] = "#{model.service_url} has recovered."
+  def service_recovered(service)
+    puts "Publishing service_downed event for #{service.service_url}"
 
-        Bushido::Event.publish({
-                                 :category => :site_monitor,
-                                 :name => :recovered,
-                                 :data => data
-                               })
-      end
+    data = service.to_hash
+    data[:human] = "#{service.service_url} has gone critical!"
 
-    end
+    Bushido::Event.publish(:category => :service,
+                           :name     => :downed,
+                           :data     => data)
+  end
+
+  def service_downed(service)
+    puts "Publishing service_recovered event for #{service.service_url}"
+
+    data = service.to_hash
+    data[:human] = "#{service.service_url} has recovered."
+
+    Bushido::Event.publish(:category => :service,
+                           :name     => :recovered,
+                           :data     => data)
   end
 end
+
